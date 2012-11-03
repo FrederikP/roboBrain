978c29db8dba5e00db5cabe677828ecc7c3b5498
Frederik Petersen <frederik@upseil.com>
2012-11-03 20:50:23 +0100

    Small fix to avoid null pointer in RobotReceiver

--- RoboBrain/src/eu/fpetersen/robobrain/communication/RobotReceiver.java
+++ RoboBrain/src/eu/fpetersen/robobrain/communication/RobotReceiver.java
@@ -34,37 +34,39 @@ public class RobotReceiver extends BroadcastReceiver {
 					.getStringExtra(AmarinoIntent.EXTRA_DEVICE_ADDRESS);
 			CommandCenter cc = CommandCenter.getCCForAddress(address);
 			Robot robot = cc.getRobot();
-			String frontPrefix = "FRONTPROX:";
-			String backPrefix = "BACKPROX:";
-			String consolePrefix = "CONSOLE:";
-			String stoppedAfterDelay = "STOPPEDAFTERDELAY";
-			if (data.startsWith(frontPrefix)) {
-				String substring = data.substring(frontPrefix.length());
-				int proxValue = Integer.parseInt(substring);
-				if (proxValue == 0) {
-					Log.v("RobotReceiver", "Front Prox returned 0");
-				} else {
-					robot.getFrontSensor().setValue(proxValue);
+			if (robot != null) {
+				String frontPrefix = "FRONTPROX:";
+				String backPrefix = "BACKPROX:";
+				String consolePrefix = "CONSOLE:";
+				String stoppedAfterDelay = "STOPPEDAFTERDELAY";
+				if (data.startsWith(frontPrefix)) {
+					String substring = data.substring(frontPrefix.length());
+					int proxValue = Integer.parseInt(substring);
+					if (proxValue == 0) {
+						Log.v("RobotReceiver", "Front Prox returned 0");
+					} else {
+						robot.getFrontSensor().setValue(proxValue);
+					}
+				} else if (data.startsWith(backPrefix)) {
+					String substring = data.substring(backPrefix.length());
+					int proxValue = Integer.parseInt(substring);
+					robot.getBackSensor().setValue(proxValue);
+				} else if (data.contains(stoppedAfterDelay)) {
+					robot.getMainMotor().delayActionDone();
+				} else if (data.startsWith(consolePrefix)) {
+					String substring = data.substring(consolePrefix.length());
+					RoboLog.log(substring);
 				}
-			} else if (data.startsWith(backPrefix)) {
-				String substring = data.substring(backPrefix.length());
-				int proxValue = Integer.parseInt(substring);
-				robot.getBackSensor().setValue(proxValue);
-			} else if (data.contains(stoppedAfterDelay)) {
-				robot.getMainMotor().delayActionDone();
-			} else if (data.startsWith(consolePrefix)) {
-				String substring = data.substring(consolePrefix.length());
-				RoboLog.log(substring);
-			}
 
-			/*
-			 * //Uncomment for debugging purposes: (Slow phone can be
-			 * overwhelmed by a high rate of data
-			 * 
-			 * Intent cIntent = new Intent(RoboBrainIntent.ACTION_OUTPUT);
-			 * cIntent.putExtra(RoboBrainIntent.EXTRA_OUTPUT, data);
-			 * context.sendBroadcast(cIntent);
-			 */
+				/*
+				 * //Uncomment for debugging purposes: (Slow phone can be
+				 * overwhelmed by a high rate of data
+				 * 
+				 * Intent cIntent = new Intent(RoboBrainIntent.ACTION_OUTPUT);
+				 * cIntent.putExtra(RoboBrainIntent.EXTRA_OUTPUT, data);
+				 * context.sendBroadcast(cIntent);
+				 */
+			}
 		}
 
 	}
