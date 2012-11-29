c354ed2d7f87766e95e9a481fd96c0c33a09a04d
Frederik Petersen <frederik@upseil.com>
2012-11-29 13:50:48 +0100

    Reverted last commit

--- RoboBrainTest/src/eu/fpetersen/robobrain/test/util/Helper.java
+++ RoboBrainTest/src/eu/fpetersen/robobrain/test/util/Helper.java
@@ -39,12 +39,7 @@ public class Helper {
 	 */
 	public static void sleepMillis(long millis) {
 		try {
-			int multiplier = 1;
-			if (System.getenv("buildonjenkins") != null
-					&& System.getenv("buildonjenkins").matches("true")) {
-				multiplier = 5;
-			}
-			Thread.sleep(millis * multiplier);
+			Thread.sleep(millis);
 		} catch (InterruptedException e) {
 			// Usually works. Is only used for tests so try again. And look at
 			// stacktrace.
